0a1625ddc0147e95491996b88c44082488ebf044
frappe <frappe@ubuntu.vm>
2018-06-30 15:24:58 +0000

    v003

---
+++ ufr/ufr/doctype/objetivos_servicio/objetivos_servicio.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, RP and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Objetivos Servicio', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ ufr/ufr/doctype/tabla_20/test_tabla_20.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+// rename this file from _test_[name] to test_[name] to activate
+// and remove above this line
+
+QUnit.test("test: Tabla 20", function (assert) {
+	let done = assert.async();
+
+	// number of asserts
+	assert.expect(1);
+
+	frappe.run_serially([
+		// insert a new Tabla 20
+		() => frappe.tests.make('Tabla 20', [
+			// values to be set
+			{key: 'value'}
+		]),
+		() => {
+			assert.equal(cur_frm.doc.key, 'value');
+		},
+		() => done()
+	]);
+
+});
