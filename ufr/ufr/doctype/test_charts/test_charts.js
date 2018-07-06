57e7198b30132db576d519d053eb9d5761d03e69
Rodrigo <>
2018-07-06 16:59:34 -0400

    v005

--- ufr/ufr/doctype/test_charts/test_charts.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2018, RP and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('test Charts', {
-	refresh: function(frm) {
-
-	}
-});
--- ufr/ufr/doctype/testdoc/testdoc.js
+++ ufr/ufr/doctype/testdoc/testdoc.js
@@ -1,41 +1,55 @@
-// Copyright (c) 2018, RP and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('TestDoc', {
+frappe.ui.form.on('graphtesting', {
 	refresh: function(frm) {
-<<<<<<< HEAD
-let chart = new frappe.Chart( "#chart", { // or DOM element
-    data: {
-      labels: ["12am-3am", "3am-6am", "6am-9am", "9am-12pm",
-      "12pm-3pm", "3pm-6pm", "6pm-9pm", "9pm-12am"],
-=======
-    }
->>>>>>> 4a03ce8ef915eb7bc68dca260423b00c1f314983
 
-  const data = {
-    labels: ["12am-3am", "3am-6pm", "6am-9am", "9am-12am",
-        "12pm-3pm", "3pm-6pm", "6pm-9pm", "9am-12am"
-    ],
-    datasets: [
-        {
-            name: "Some Data", type: "bar",
-            values: [25, 40, 30, 35, 8, 52, 17, -4]
-        },
-        {
-            name: "Another Set", type: "line",
-            values: [25, 50, -10, 15, 18, 32, 27, 14]
-        }
-    ]
-}
+$.getScript("https://cdn.jsdelivr.net/npm/frappe-charts@1.1.0/dist/frappe-charts.min.iife.js",function(){
 
-const chart = new frappe.Chart("#chart", {  // or a DOM element,
-                                            // new Chart() in case of ES6 module with above usage
-    title: "My Awesome Chart",
-    data: data,
-    type: 'axis-mixed', // or 'bar', 'line', 'scatter', 'pie', 'percentage'
-    height: 250,
-    colors: ['#7cd6fd', '#743ee2']
-})
-		
+let chart = new frappe.Chart( "#frost-chart", { // or DOM element
+	data: {
+	labels: ["12am-3am", "3am-6am", "6am-9am", "9am-12pm",
+		"12pm-3pm", "3pm-6pm", "6pm-9pm", "9pm-12am"],
+
+	datasets: [
+		{
+			name: "Some Data", chartType: 'bar',
+			values: [25, 40, 30, 35, 8, 52, 17, -4]
+		},
+		{
+			name: "Another Set", chartType: 'bar',
+			values: [25, 50, -10, 15, 18, 32, 27, 14]
+		},
+		{
+			name: "Yet Another", chartType: 'line',
+			values: [15, 20, -3, -15, 58, 12, -17, 37]
+		}
+	],
+
+	yMarkers: [{ label: "Marker", value: 70,
+		options: { labelPos: 'left' }}],
+	yRegions: [{ label: "Region", start: -10, end: 50,
+		options: { labelPos: 'right' }}]
+	},
+
+	title: "My Awesome Chart",
+	type: 'axis-mixed', // or 'bar', 'line', 'pie', 'percentage'
+	height: 300,
+	colors: ['purple', '#ffa3ef', 'light-blue'],
+
+	tooltipOptions: {
+		formatTooltipX: d => (d + '').toUpperCase(),
+		formatTooltipY: d => d + ' pts',
+	}
+  });
+
+  chart.export();
+
+					
+
+
+								
+
+
+
+});
+
 	}
 });
