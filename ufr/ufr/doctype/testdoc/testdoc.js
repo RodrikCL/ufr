b56026f380e114d1e3e4e3173b418d379bb454a3
Rodrigo <>
2018-07-11 18:19:32 -0400

    v007

--- ufr/ufr/doctype/testdoc/testdoc.js
+++ ufr/ufr/doctype/testdoc/testdoc.js
@@ -1,45 +1,48 @@
 frappe.ui.form.on(‘TestDoc’, {
 refresh: function(frm) {
 
-$.getScript(“https://cdn.jsdelivr.net/npm/frappe-charts@1.1.0/dist/frappe-charts.min.iife.js” ,function(){
+$.getScript(“ https://cdn.jsdelivr.net/npm/frappe-charts@1.1.0/dist/frappe-charts.min.iife.js ”
+																								, function(){
+	let chart = new frappe.Chart( “#chart”, {
+											data: {
+	
+													labels: [“12am-3am”, “3am-6am”, “6am-9am”, “9am-12pm”,“12pm-3pm”, “3pm-6pm”, “6pm-9pm”, “9pm-12am”],
+												  datasets: [
+																{
+																name: "Some Data", chartType: 'bar',
+															values: [25, 40, 30, 35, 8, 52, 17, -4]
+																},
+																{
+																name: "Another Set", chartType: 'bar',
+															  values: [25, 50, -10, 15, 18, 32, 27, 14]
+																},
+																{
+																name: "Yet Another", chartType: 'line',
+															  values: [15, 20, -3, -15, 58, 12, -17, 37]
+																}
+															],
 
-let chart = new frappe.Chart( “#chart”, {
-data: {
-labels: [“12am-3am”, “3am-6am”, “6am-9am”, “9am-12pm”,
-“12pm-3pm”, “3pm-6pm”, “6pm-9pm”, “9pm-12am”],
+													yMarkers: [{ label: "Marker", value: 70,
+																options: { labelPos: 'left' }}],
+												    yRegions: [{ label: "Region", start: -10, end: 50,
+															   options: { labelPos: 'right' }}]
+													},
 
-  datasets: [
-    {
-      name: "Some Data", chartType: 'bar',
-      values: [25, 40, 30, 35, 8, 52, 17, -4]
-    },
-    {
-      name: "Another Set", chartType: 'bar',
-      values: [25, 50, -10, 15, 18, 32, 27, 14]
-    },
-    {
-      name: "Yet Another", chartType: 'line',
-      values: [15, 20, -3, -15, 58, 12, -17, 37]
-    }
-  ],
+				title: "Chart",
+				type: 'axis-mixed',
+				height: 300,
+				colors: ['purple', '#ffa3ef', 'light-blue'],
 
-  yMarkers: [{ label: "Marker", value: 70,
-    options: { labelPos: 'left' }}],
-  yRegions: [{ label: "Region", start: -10, end: 50,
-    options: { labelPos: 'right' }}]
-},
+				tooltipOptions: {
+								formatTooltipX: d => (d + '').toUpperCase(),
+								formatTooltipY: d => d + ' pts',
+								}
+											}
+								);
 
-title: "Chart",
-type: 'axis-mixed',
-height: 300,
-colors: ['purple', '#ffa3ef', 'light-blue'],
-
-tooltipOptions: {
-  formatTooltipX: d => (d + '').toUpperCase(),
-  formatTooltipY: d => d + ' pts',
+																											}
+			);
+					}
 }
-});
-
-});
-}
-});
+				
+);
